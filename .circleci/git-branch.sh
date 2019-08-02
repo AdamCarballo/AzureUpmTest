b3dd8bf30a3e66ef30c48a92ca4900bde64e822b
Àdam Carballo <adam.carballo@outfit7.com>
2019-08-02 09:10:03 +0200

    Source variables

--- .circleci/git-branch.sh
+++ .circleci/git-branch.sh
@@ -7,5 +7,10 @@ set -o xtrace
 git checkout --track origin/upm
 git checkout master
 git subtree split --prefix=Assets/$UPM_DIRECTORY --branch upm
+
+echo $VERSION
+source $BASH_ENV
+echo $VERSION
+
 git tag $VERSION upm
 git push origin upm --tags
