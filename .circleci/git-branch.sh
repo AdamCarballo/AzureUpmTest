886783d4f1900dd603a4220e4ea8d4ba0baa6a7a
Àdam Carballo <adam.carballo@outfit7.com>
2019-08-02 09:35:54 +0200

    Let's try

--- .circleci/git-branch.sh
+++ .circleci/git-branch.sh
@@ -4,14 +4,16 @@ set -o nounset
 set -o errexit
 set -o xtrace
 
-git checkout --track origin/upm
-git checkout master
-git subtree split --prefix=Assets/$UPM_DIRECTORY --branch upm
-
-echo $VERSION
-
+# Fix VERSION variable and add V prefix
 TRIMMED_VERSION="v$(echo "${VERSION}" | tr -d '[:space:]')"
-echo $TRIMMED_VERSION
 
+# Checkout upm
+git checkout --track origin/upm
+# Go back to master
+git checkout master
+# Split master by the directory and commit to upm
+git subtree split --prefix=Assets/$UPM_DIRECTORY --branch upm
+# Tag the last upm commit
 git tag $TRIMMED_VERSION upm
+# Push to upm with tag
 git push origin upm --tags
