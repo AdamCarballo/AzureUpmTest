7b5977cb0897b14f3da06939a5d605e5fc49e339
Àdam Carballo <adam.carballo@outfit7.com>
2019-08-02 09:02:36 +0200

    Not everything on script

--- .circleci/git-branch.sh
+++ .circleci/git-branch.sh
@@ -4,9 +4,6 @@ set -o nounset
 set -o errexit
 set -o xtrace
 
-echo 'export VERSION=$(cat ../Assets/$upm_DIRECTORY/package.json | grep version | head -1 | cut -d \  -f 4 | sed "s/[\",]//g")' >> $BASH_ENV
-echo ${VERSION}
-
 git checkout --track origin/upm
 git subtree split --prefix=Assets/$upm_DIRECTORY --branch upm
 git tag $VERSION upm
